82acf35b5b335cb191f894d4724f0149435d675b
Konstantin <ksharutin@gmail.com>
2015-06-08 20:00:43 +0300

    Update LA.h

--- LA.h
+++ LA.h
@@ -1,100 +1,20 @@
-#include<iostream>
-#include<conio.h>
-
+#include <iostream>
 using namespace std;
-
-class LA
+ 
+int main(int argc, char* argv[])
 {
-protected:
-	char model[64];
-	int aEngine;
-	int power;
-public:
-	LA(char *, int, int);
-	void ShowLA(void);
-	void SetPowerandaEngines();
-};
-
-LA::LA(char *model, int aEngine, int power)
-{
-	strcpy_s(LA::model, model);
-	LA::aEngine = aEngine;
-	LA::power = power;
-}
-
-void LA::ShowLA()
-{
-	cout << endl;
-	cout << "Model: " << model << endl;
-	cout << "Number of air-Engines: " << aEngine << endl;
-	cout << "power: " << power << endl;
-}
-
-void LA::SetPowerandaEngines()
-{
-	cout << "Enter a power: ";
-	cin >> power;
-	cout << "Enter a number of air-Engines: ";
-	cin >> aEngine;
-}
-//AIRPLANE
-class Airplane : public LA
-{
-	float Number;
-public:
-	Airplane(char *, int, int, float);
-	void ShowAirplane();
-	void SetModelAndNumber(void);
-};
-
-Airplane::Airplane(char *model, int aEngine, int power, float Number) : LA(model, aEngine, power)
-{
-Airplane:Number = Number;
-}
-
-void Airplane::ShowAirplane(void)
-{
-	cout << endl;
-	ShowLA();
-	cout << "Number of places in Airplane: " << Number << endl;
-}
-
-void Airplane::SetModelAndNumber(void)
-{
-	cout << endl << "Enter a model of Airplane: ";
-	cin.get();
-	cin.getline(model, 64);
-	//gets(model);
-	cout << "Enter a number of places: ";
-	cin >> Number;
-}
-//CHOPPER
-class Chopper : public LA
-{
-	float Number;
-public:
-	Chopper(char *, int, int, float);
-	void ShowChopper(void);
-	void SetModelAndNumber(void);
-};
-
-Chopper::Chopper(char *model, int aEngine, int power, float Number) : LA(model, aEngine, power)
-{
-	Chopper::Number = Number;
-}
-
-void Chopper::SetModelAndNumber(void)
-{
-	cout << endl << "Enter a model of Chopper: ";
-	cin.get();
-	cin.getline(model, 64);
-	//gets(model);
-	cout << "Enter a number of places: ";
-	cin >> Number;
-}
-void Chopper::ShowChopper(void)
-{
-	cout << endl;
-	ShowLA();
-	cout << "Number of places in Chopper: " << Number << endl;
+    int var1 = 123; // инициализация переменной var1 числом 123
+    int var2 = 99; // инициализация переменной var2 числом 99
+    int *ptrvar1 = &var1; // указатель на переменную var1
+    int *ptrvar2 = &var2; // указатель на переменную var2
+    cout << "var1    = " << var1 << endl;
+    cout << "var2    = " << var2 << endl;
+    cout << "ptrvar1 = " << ptrvar1 << endl;
+    cout << "ptrvar2 = " << ptrvar2 << endl;
+    if (ptrvar1 > ptrvar2) // сравниваем значения указателей, то есть адреса переменных
+        cout << "ptrvar1 > ptrvar2" << endl;
+    if (*ptrvar1 > *ptrvar2) // сравниваем значения переменных, на которые ссылаются указатели
+        cout << "*ptrvar1 > *ptrvar2" << endl;
+    system("pause");
+    return 0;
 }
